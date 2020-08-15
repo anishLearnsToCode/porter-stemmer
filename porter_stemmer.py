016ccc3483fbb4d55839d08cc16a74501032cdf5
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-08-16 00:01:01 +0530

    edits the init method

--- porter_stemmer.py
+++ porter_stemmer.py
@@ -1,44 +1,9 @@
-#!/usr/bin/env python
-
-"""Porter Stemming Algorithm
-This is the Porter stemming algorithm, ported to Python from the
-version coded up in ANSI C by the author. It may be be regarded
-as canonical, in that it follows the algorithm presented in
-
-Porter, 1980, An algorithm for suffix stripping, Program, Vol. 14,
-no. 3, pp 130-137,
-
-only differing from it at the points maked --DEPARTURE-- below.
-
-See also http://www.tartarus.org/~martin/PorterStemmer
-
-The algorithm as described in the paper could be exactly replicated
-by adjusting the points of DEPARTURE, but this is barely necessary,
-because (a) the points of DEPARTURE are definitely improvements, and
-(b) no encoding of the Porter stemmer I have seen is anything like
-as exact as this version, even with the points of DEPARTURE!
-
-Vivake Gupta (v@nano.com)
-
-Release 1: January 2001
-
-Further adjustments by Santiago Bruno (bananabruno@gmail.com)
-to allow word input not restricted to one word per line, leading
-to:
-
-release 2: July 2008
-"""
-
-import sys
-
-
 class PorterStemmer:
 
     def __init__(self):
-        """The main part of the stemming algorithm starts here.
-        word is a buffer holding a word to be stemmed. The letters are in the range
-        [start, offset ... offset + 1) ... ending at end.
-        """
+        """The word is a buffer holding a word to be stemmed. The letters are in the range
+        [start, offset ... offset + 1) ... ending at end."""
+
         self.vowels = ('a', 'e', 'i', 'o', 'u')
         self.word = ''
         self.end = 0
@@ -49,7 +14,7 @@ class PorterStemmer:
         return letter in self.vowels
 
     def is_consonant(self, index):
-        """cons(i) is TRUE <=> b[i] is a consonant."""
+        """:returns True if word[index] is a consonant."""
         if self.is_vowel(self.word[index]):
             return False
         if self.word[index] == 'y':
